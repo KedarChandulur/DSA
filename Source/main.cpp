f47abdce53994e3df34fc843165cada243a5c1e2
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-04-17 12:12:38 -0500

    Updated main to run Catalan Top Down example.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "CatalanTopDown.h"
 
 int main()
 {
     std::cout << "Main is running" << std::endl;
+
+    // --- Catalan Top Down example ---
+
+    CatalanTopDown catalanTopDown;
+
+	const uint32_t value = catalanTopDown.GetInput();
+
+	const uint32_t output = catalanTopDown.Calculate(value);
+
+	std::cout << "\nNumber of Multiplications are: " << output << std::endl;
+
+    // --- Catalan Top Down example ---
 
     return 0;
 }
