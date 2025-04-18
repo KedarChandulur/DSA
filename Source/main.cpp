b22fed9735320143fcdd149e42851abf611fe953
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-04-17 23:23:31 -0500

    Updated main with min no of bills class.

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,10 +4,12 @@
 #include "CatalanTopDown.h"
 #include "CatalanMemoized.h"
 #include "MaxProfit.h"
+#include "MinNoOfBills.h"
 
 #define CATALAN_TOP_DOWN_TEST false
 #define CATALAN_MEMOIZED_TEST false
-#define MAX_PROFIT_TEST true
+#define MAX_PROFIT_TEST false
+#define MIN_NO_OF_BILLS_TEST true
 
 int main()
 {
@@ -77,6 +79,38 @@ int main()
     #endif
 
     // --- Max Profit example ---
+
+    // --- Min No Of Bills example ---
+
+    #if MIN_NO_OF_BILLS_TEST
+    {
+        MinNoOfBills minNoOfBills;
+
+        int target = 122;
+        std::vector<int> denomination;
+
+        if(minNoOfBills.GetInput(denomination, target))
+        {
+            const std::vector<int> solution = minNoOfBills.FindMinDenomination(target, denomination);
+    
+            std::cout << "\nMinimum number bills required: " << solution.size() << std::endl;
+            std::cout << "Following is minimal number of bills for " << target << ": ";
+            
+            for (int i = 0; i < solution.size(); i++)
+            {
+                std::cout << solution[i] << " ";
+            }
+        
+            std::cout << std::endl;
+        }
+        else
+        {
+            std::cout << "\nMin No Of Bills program failed\n";
+        }
+    }
+    #endif
+
+    // --- Min No Of Bills example ---
 
     std::cout << "\nDSA Program Complete..." << std::endl;
 
