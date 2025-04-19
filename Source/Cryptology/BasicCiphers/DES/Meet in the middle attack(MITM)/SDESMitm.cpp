21e08a72ff5196f3eea8e082f9266f3b08043126
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-04-19 02:15:34 -0500

    Added the SDESConstants namespace

--- Source/Cryptology/BasicCiphers/DES/Meet in the middle attack(MITM)/SDESMitm.cpp
+++ Source/Cryptology/BasicCiphers/DES/Meet in the middle attack(MITM)/SDESMitm.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 #include <unordered_map>
 
+#include "SDESConstants.h"
 #include "SDESMitm.h"
 
-//static uint8_t s1BoxArray1[] = { 0b101, 0b010, 0b001, 0b110, 0b011, 0b100, 0b111, 0b000 };
-//static uint8_t s1BoxArray2[] = { 0b001, 0b100, 0b110, 0b010, 0b000, 0b111, 0b101, 0b011 };
-//static uint8_t s2BoxArray1[] = { 0b100, 0b000, 0b110, 0b101, 0b111, 0b001, 0b011, 0b010 };
-//static uint8_t s2BoxArray2[] = { 0b101, 0b011, 0b000, 0b111, 0b110, 0b010, 0b001, 0b100 };
-
-static uint8_t s1BoxArray1[] = { 5, 2, 1, 6, 3, 4, 7, 0 };
-static uint8_t s1BoxArray2[] = { 1, 4, 6, 2, 0, 7, 5, 3 };
-static uint8_t s2BoxArray1[] = { 4, 0, 6, 5, 7, 1, 3, 2 };
-static uint8_t s2BoxArray2[] = { 5, 3, 0, 7, 6, 2, 1, 4 };
+using namespace SDESConstants;
 
 SDES_MITM::SDES_MITM()
 {
