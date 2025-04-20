6ae03558d66c1c465b9a583e02ff465d8d6955c8
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-04-19 19:25:40 -0500

    Updated Encrption class
    Added common methods in Encryption class.
    Encryption will be the base class for decryption and MITM.

--- Source/Cryptology/BasicCiphers/DES/Simple DES - Encryption/SDESencrypt.cpp
+++ Source/Cryptology/BasicCiphers/DES/Simple DES - Encryption/SDESencrypt.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
-static uint16_t s1BoxArray1[] = { 0b101, 0b010, 0b001, 0b110, 0b011, 0b100, 0b111, 0b000 };
-static uint16_t s1BoxArray2[] = { 0b001, 0b100, 0b110, 0b010, 0b000, 0b111, 0b101, 0b011 };
-static uint16_t s2BoxArray1[] = { 0b100, 0b000, 0b110, 0b101, 0b111, 0b001, 0b011, 0b010 };
-static uint16_t s2BoxArray2[] = { 0b101, 0b011, 0b000, 0b111, 0b110, 0b010, 0b001, 0b100 };
+#include "SDESConstants.h"
+#include "SDESencrypt.h"
 
-uint8_t ExpanderFunction(uint8_t& RValue)
+using namespace SDESConstants;
+
+SDES_Encrypt::SDES_Encrypt(const bool printProgramStart)
+{
+	if(printProgramStart)
+	{
+		std::cout << "\nWelcome to Simple DES Encryption Program\n" << std::endl;
+	}
+}
+
+uint8_t SDES_Encrypt::ExpanderFunction(uint8_t& RValue) const
 {
 	char bit1 = (RValue & 0b100000) >> 5;
 	char bit2 = (RValue & 0b010000) >> 4;
@@ -19,7 +27,7 @@ uint8_t ExpanderFunction(uint8_t& RValue)
 	return expandedR;
 }
 
-void S1Box(uint8_t& l4BitValue)
+void SDES_Encrypt::S1Box(uint8_t& l4BitValue) const
 {
 	uint8_t fourBitValue = l4BitValue & 0b1111;
 
@@ -37,7 +45,7 @@ void S1Box(uint8_t& l4BitValue)
 	}
 }
 
-void S2Box(uint8_t& r4BitValue)
+void SDES_Encrypt::S2Box(uint8_t& r4BitValue) const
 {
 	uint8_t fourBitValue = r4BitValue & 0b1111;
 
@@ -55,72 +63,66 @@ void S2Box(uint8_t& r4BitValue)
 	}
 }
 
-uint16_t PerformRoundOperations(uint8_t& lValue, uint8_t& RValue, uint16_t& key)
+void SDES_Encrypt::GenerateSubKeys(uint16_t* subkeys, const uint16_t& key) const
+{
+	subkeys[0] = (key & 0b111111110) >> 1;
+	subkeys[1] = ((key & 0b011111111) << 0) + ((key & 0b000000000) >> 9);
+	subkeys[2] = ((key & 0b001111111) << 1) + ((key & 0b100000000) >> 8);
+	subkeys[3] = ((key & 0b000111111) << 2) + ((key & 0b110000000) >> 7);
+}
+
+void SDES_Encrypt::PerformRound(uint8_t& lValue, uint8_t& RValue, uint16_t* subkeys, uint16_t& output, uint16_t subkeyNumber) const
+{
+	uint8_t r8BitValue = ExpanderFunction(RValue);
+
+	uint16_t refinedKey = subkeys[subkeyNumber];
+
+	r8BitValue ^= refinedKey;
+
+	uint8_t l4BitValue = (r8BitValue & 0b11110000) >> 4;
+	uint8_t r4BitValue = (r8BitValue & 0b1111) >> 0;
+
+	S1Box(l4BitValue);
+	S2Box(r4BitValue);
+
+	l4BitValue = l4BitValue << 3;
+
+	uint8_t out = 0;
+	out = lValue ^ (l4BitValue + r4BitValue);
+
+	lValue = RValue;
+	RValue = out;
+
+	output = lValue;
+	output = output << 6;
+	output += RValue;
+}
+
+uint16_t SDES_Encrypt::PerformEncryptionRoundOperations(uint8_t& lValue, uint8_t& RValue, uint16_t& key) const
 {
 	uint16_t output = 0;
+
+	uint16_t subkeys[4];
+
+	GenerateSubKeys(subkeys, key);
+
 	for (uint16_t i = 0; i < 4; i++)
 	{
-		uint8_t r8BitValue = ExpanderFunction(RValue);
-
-		uint16_t refinedKey = 0;
-		if (i == 0)
-		{
-			refinedKey = (key & 0b111111110) >> 1;
-		}
-		if (i == 1)
-		{
-			refinedKey = ((key & 0b011111111) << 0) + ((key & 0b000000000) >> 9);
-		}
-		if (i == 2)
-		{
-			refinedKey = ((key & 0b001111111) << 1) + ((key & 0b100000000) >> 8);
-		}
-		if (i == 3)
-		{
-			refinedKey = ((key & 0b000111111) << 2) + ((key & 0b110000000) >> 7);
-		}
-
-		r8BitValue ^= refinedKey;
-
-		uint8_t l4BitValue = (r8BitValue & 0b11110000) >> 4;
-		uint8_t r4BitValue = (r8BitValue & 0b1111) >> 0;
-
-		S1Box(l4BitValue);
-		S2Box(r4BitValue);
-
-		l4BitValue = l4BitValue << 3;
-
-		uint8_t out = 0;
-		out = lValue ^ (l4BitValue + r4BitValue);
-		
-		lValue = RValue;
-		RValue = out;
-
-		output = lValue;
-		output = output << 6;
-		output += RValue;
+		PerformRound(lValue, RValue, subkeys, output, i);
 	}
 
 	return output;
 }
 
-void Encrypt(uint16_t plaintext, uint16_t key)
+void SDES_Encrypt::PerformEncryption(uint16_t plaintext, uint16_t key, uint16_t& output) const
 {
 	uint8_t lValue = (plaintext >> 6) & 0x3F;
 	uint8_t rValue = (plaintext) & 0x3F;
-
-	uint16_t output = PerformRoundOperations(lValue, rValue, key);
-
-	std::cout << "\nCipher Text is: " << (int)output << std::endl;
+	output = PerformEncryptionRoundOperations(lValue, rValue, key);
 }
 
-int main()
+void SDES_Encrypt::GetInput(uint16_t& plaintext, uint16_t& key) const
 {
-	uint16_t plaintext = 0;
-	uint16_t key = 0;
-
-	std::cout << "Encryption using SDES\n" << std::endl;
-
 	std::cout << "Enter PlainText:" << std::endl;
 
 	std::cin >> plaintext;
@@ -128,8 +130,17 @@ int main()
 	std::cout << "Enter Key:" << std::endl;
 
 	std::cin >> key;
+}
 
-	Encrypt(plaintext, key);
+void SDES_Encrypt::RunDemo() const
+{
+	uint16_t plaintext = 0;
+	uint16_t key = 0;
+	uint16_t output;
 
-	return 0;
+	GetInput(plaintext, key);
+
+	PerformEncryption(plaintext, key, output);
+
+	std::cout << "\nCipher Text is: " << (int)output << std::endl;
 }
